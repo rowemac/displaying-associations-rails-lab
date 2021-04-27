21e46264f6baa56f3f6b6e7258cf8fa79586f10c
Mac Rowe <m.rowe42@gmail.com>
2021-04-27 09:23:12 -0400

    commit tree 8e614a5a25b484c20875a4c73d6c5415b004c965

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,12 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
+
+  def artist_name
+    self.artist.name
+  end
+
+  def artist_and_title
+    artist_name + " - " + self.title
+  end
+
 end
